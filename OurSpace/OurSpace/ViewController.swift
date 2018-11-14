45be89296456cfd8355b4647d54fd99d2371eeda
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2018-11-14 09:53:25 -0800

    initial project added

---
+++ OurSpace/OurSpace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurSpace
+//
+//  Created by Tommy Tran on 11/14/18.
+//  Copyright © 2018 Tommy Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
